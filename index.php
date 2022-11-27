9477d30688fefd316e6366ea7382e7a6d401fdbe
TheRiom1 <96839945+TheRiom1@users.noreply.github.com>
2022-11-27 13:36:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+</head>
+<body>
+    <nav>
+        <div class="container nav_container">
+            <a href="index.html" class="nav_logo">BLOG</a>
+            <ul class="nav_items">
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="about.html">Blog</a></li>
+                <li><a href="services.html">Blog</a></li>
+                <li><a href="contact.html">Blog</a></li>
+                <li><a href="signin.html">Blog</a></li>
+                <li class="nav_profile">
+                <div class="avatar">
+                    <img src="./images/avatar1.jpg">
+                </div>    
+                <ul>
+                <li><a href="dashboard.html">Dashboard</a></li>
+                <li><a href="logout.html">Logut</a></li>
+                </ul>
+            </ul>
+            <button id="open_nav-btn"><i class="uil uil-bars"></i></button>
+            <button id="close_nav-btn"><i class="uil uil-multiply"></i></button>
+        </div>
+    </nav>
+</body>
+</html>
