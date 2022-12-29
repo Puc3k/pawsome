18fc28e82268786cf019050097f475cdde57e58c
Rafał Andrzejewski <>
2022-12-29 16:06:42 +0100

    Frontend Quiz

--- templates/pages/index.php
+++ templates/pages/index.php
@@ -1,25 +1,24 @@
 <main>
 
-<div class="game justify-content-md-middle">
+  <div class="quiz-index justify-content-md-middle">
     <div class="container position-absolute top-50 start-50 translate-middle text-center">
-            <h1 class="text-light">Najlepsza rasa psa</h1>
-            <p class="text-light">Gra z wybieraniem najlepszej rasy psa to zabawna i wciągająca gra, w której gracze wybierają jedną z dwóch dostępnych ras psów. W każdej rundzie gracze wybierają jedną z dwóch ras.</p>
-            <p><a class="btn btn-lg btn-light" href="#">Graj teraz</a></p>
-            </div>
-        </div>
-      
+      <h1 class="text-light">Najlepsza rasa psa</h1>
+      <p class="text-light">Gra z wybieraniem najlepszej rasy psa to zabawna i wciągająca gra, w której gracze wybierają jedną z dwóch dostępnych ras psów. W każdej rundzie gracze wybierają jedną z dwóch ras.</p>
+      <p><a class="btn btn-lg btn-light" href="../../public/quiz">Graj teraz</a></p>
+    </div>
+  </div>
+
   <div class="container">
     <h1 class="text-center my-2">Ciekawostka na dziś </h1>
     <hr>
 
     <div class="row">
-      <div class="col-md-7">
+      <div class="col-lg-7">
         <h2 class="fw-normal lh-1">Ciekawostka</h2>
         <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       </div>
-      <div class="col-md-5">
-          <img src="../../images/ciekawostka.jpg" width="500">
-        </div>
+      <div class="col-lg-5 dog-image-curiosity"style="background-image: url(../../images/ciekawostka.jpg);">
+      </div>
     </div>
 
     <hr>
---
+++ templates/pages/quiz.php
@@ -0,0 +1,30 @@
+<main>
+    <div class="quiz">
+        <div class="container">
+            <h1 class="text-center text-light pt-4">Najlepsza rasa psa</h1>
+            <h3 class="text-center text-light mt-2">1/16</h3>
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="card shadow-sm m-1">
+                        <div class="dog-image" style="background-image: url(../../images/dog1.jpg);">
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text text-center">Przykładowa rasa #1</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 p-2">
+                <h1 class="text-center text-light position-relative top-50 start-50 translate-middle">VS</h1>
+                </div>
+                <div class="col-lg-5">
+                    <div class="card shadow-sm m-1 mb-5">
+                        <div class="dog-image" style="background-image: url(../../images/dog2.jpg);">
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text text-center">Przykładowa rasa #2</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</main>
