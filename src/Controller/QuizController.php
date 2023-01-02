6b906267afa54c356177ce251481e539d2918f0d
Puc3k <kacperplocienniczak20@gmail.com>
2023-01-02 16:21:23 +0100

    fix: database config init

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -3,9 +3,15 @@
 namespace App\Controller;
 
 use App\Core\Controller;
+use App\Core\Request;
+use App\Helpers\Database;
+use App\Helpers\Session;
 
 class QuizController extends Controller
 {
+    private int $round = 0;
+    private int $maxRound = 16;
+
     public function index()
     {
         $this->checkIsUserLogged('admin');
@@ -14,7 +20,26 @@ class QuizController extends Controller
 
     public function quiz()
     {
+        dd($this->request->hasPost());
+        Session::init();
+        $round = Session::exists('quiz-round');
+        $this->round = $round ? Session::get('quiz-round') : 1;
+        if ($this->round == 1) {
+            $db = Database::getInstance()->getConnection();
+            $query = $db->prepare('SELECT * FROM breed_images ORDER BY rand() limit :limit');
+            $query->execute([
+                'limit' => $this->maxRound
+            ]);
+
+            $data = $query->fetchAll();
+            dd($data);
+        }
+        Session::put('quiz-round', $this->round);
+
+        dd(Session::get('quiz-round'));
+        dd($this->round);
+
         $this->view->render('quiz');
     }
-    
+
 }
--- src/Helpers/Config.php
+++ src/Helpers/Config.php
@@ -6,10 +6,10 @@ class Config
 {
     public static function getConfig()
     {
-        $config = dirname(__DIR__, 2) . '\config\config.php';
+        $config = dirname(__DIR__, 2) . '/config/config.ini';
 
         if (is_file($config)) {
-            return require_once($config);
+            return parse_ini_file($config, true);
         }
         return [];
     }
