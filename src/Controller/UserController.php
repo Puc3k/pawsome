e47cde0c4d3327734c923c44f1ce13322e4a9f8a
Rafał Andrzejewski <>
2023-01-05 23:23:10 +0100

    Added UsersList

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,25 @@
 namespace App\Controller;
 
 use App\Core\Controller;
+use App\Helpers\Auth;
 use App\Model\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $userId = User::gerUserIdFromSession();
+        $userId = User::getUserIdFromSession();
 
         $this->view->render('user-profile');
+    }
+
+    public function getUsersList()
+    {
+        if (Auth::admin()) {
+            $usersList = User::getAllUsers();
+            $this->view->render('users-list', ['usersList' => $usersList]);
+        }
+        $this->view->render('index');
     }
 
 }
